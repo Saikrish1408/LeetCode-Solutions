03f17f2b1dadfc4da970a7f66b76f5bfa7cc367d
Sai Ram Krishna <105543330+Saikrish1408@users.noreply.github.com>
2024-07-13 16:17:12 +0530

    LeetCode Solution

---
+++ ConsecutiveOdd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ThreeConsecutiveOddLCDCACCEPTED {
+    public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+        int n = scannerObject.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = scannerObject.nextInt();
+        }
+        System.out.println(isConsecutiveOdd(arr));
+        scannerObject.close();
+    }
+
+    public static boolean isConsecutiveOdd(int[] arr) {
+        for(int i = 0 ; i < arr.length - 2 ; i++) {
+            if((arr[i]%2 != 0) && (arr[i + 1]%2 != 0) && (arr[i + 2]%2 != 0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
